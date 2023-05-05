dcaac34f4016f25d4bb5ff7d7e4c9f994db8a775
favorhau <favorhau@outlook.com>
2023-05-05 16:50:27 +0800

    feat: add time trigger

--- trigger/trigger.js
+++ trigger/trigger.js
@@ -74,12 +74,38 @@ const fuckCiu = async (threadIndex ) => {
     })
 }
 
-const INTERVAL = 500;
-// 请求的时间间隔，单位为毫米
 
+const INTERVAL = 1000;
+// 请求的时间间隔，单位为毫米，建议设置为1000
 
-setInterval(()=>fuckCiu(0), INTERVAL);
+// 时
+const HH = 12;
+// 分
+const MM = 30;
+// 秒
+const SS = 0;
 
-setInterval(()=>fuckCiu(1), INTERVAL);
+//时间补偿，单位为秒，也就是说比如说我设定12点30 00秒 ，那么我12点29 58秒到 12点 30分 02 秒前后时间都会狂发请求
+const offset = 2;
 
-setInterval(()=>fuckCiu(2), INTERVAL);
+const timeTrigger = () => {
+    const now = new Date()
+    const hours = now.getHours();
+    const mins = now.getMinutes();
+    const sec = now.getSeconds();
+    
+    const cur = hours * 60 * 60 + mins * 60 + sec;
+    const tri = HH * 60 * 60 + MM * 60 + SS;
+    
+    return Math.abs(cur-tri)<=offset;
+}
+
+console.log('下一次抢球时间为 ', `${HH}: ${MM}: ${SS}`)
+
+//这个是多线程操作，现在有3个线程，如果想要多个线程，可以复制粘贴 setInterval(()=>timeTrigger()?fuckCiu(1):'', INTERVAL); 
+// 其中这里的 fuckCiu(1) 这个 1 只是给他编号，你可以改为随便一个数字
+setInterval(()=>timeTrigger()?fuckCiu(0):'', INTERVAL);
+
+setInterval(()=>timeTrigger()?fuckCiu(1):'', INTERVAL);
+
+setInterval(()=>timeTrigger()?fuckCiu(2):'', INTERVAL);
